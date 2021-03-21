48dbf9448729cc5eee82c062e6bcb6c336bf4816
McCray <tubach75@hotmail.com>
2021-03-21 14:06:19 -0400

    patch and delete issues

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -25,9 +25,32 @@ class GigsController < ApplicationController
     end
 
     get '/gigs/:id' do
+        #binding.pry
         @gig = Gig.find_by_id(params[:id])
         erb :'gigs/show'
     end
 
+    get '/gigs/:id/edit' do
+        @gig = Gig.find_by_id(params[:id])
+        erb :'gigs/edit'
+    end
+
+
+    post '/gigs/:id' do
+        @gig = Gig.find_by_id(params[:id])
+        #@gig.update(venue: params[:venue], date: params[:date], time: params[:time])
+        @gig.update(venue: params[:venue], date: params[:date], time: params[:time])
+        redirect "/gigs/#{@gig.id}"
+    end
+
+    post '/gigs/gigs/:id' do
+        @gig = Gig.find_by(id: params[:id])
+        @gig.destroy
+        if Gig.all.size == 0
+            redirect '/gigs/new'
+        else
+            redirect '/gigs'
+        end
+    end
 
 end
